a55a964e4a2274a29f2b9f9cada5b3e73088c609
wildcard329 <enkiduwildman@gmail.com>
2020-04-17 08:39:37 -0700

    initialized database

--- data/migrations/20200417081551_project-initializer.js
+++ data/migrations/20200417081551_project-initializer.js
@@ -1,8 +1,53 @@
 
 exports.up = function(knex) {
-  
+  return (
+      knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments();
+            tbl.string('name', 255)
+                .notNullable()
+                .unique();
+            tbl.string('description', 255);
+            tbl.boolean('completed', false)
+                .notNullable();
+        })
+        .createTable('resources', tbl => {
+            tbl.increments();
+            tbl.string('name', 255)
+                .notNullable();
+            tbl.string('description')
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments();
+            tbl.string('description', 255)
+                .notNullable();
+            tbl.string('notes', 255);
+            tbl.string('completed', false)
+                .notNullable();
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+        })
+        .createTable('project_resources', tbl => {
+            tbl.increments();
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('projects.id');
+            tbl.integer('resource_id')
+                .unsigned()
+                .notNullable()
+                .references('resources.id');
+        })
+  )
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
 };
