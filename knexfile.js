05de47f785c4a110683c053400ceec6484d6c59d
wildcard329 <enkiduwildman@gmail.com>
2020-04-17 08:17:37 -0700

    initialized project, initialized knex files

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/projects.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
+  }
+};
