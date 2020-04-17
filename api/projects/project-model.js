77a19ad9741cd3e8f8dc1e784cf90d9e660a174f
wildcard329 <enkiduwildman@gmail.com>
2020-04-17 08:59:52 -0700

    set up project routes and server and index files

---
+++ api/projects/project-model.js
@@ -0,0 +1,38 @@
+const db = require('../data/db-config.js')
+module.exports = {
+    find, 
+    findById,
+    add,
+    update,
+    remove,
+    findTasks
+}
+
+function find() {
+    return db('projects')
+}
+
+function findById(id) {
+    return db('projects').where({id}).first();
+}
+
+function add(project) {
+    return db('projects')
+    .insert(project, 'id')
+    .then(id => {
+        return findById(id);
+    })
+}
+
+function update(changes, id) {
+    return db('projects')
+        .where({id})
+        .update(changes)
+}
+
+function remove(id) {
+    return db('projects as p')
+        .join('tasks as t', 'p.id', 't.id')
+        .select('p.name', 't.description as task', 't.notes')
+        .where({'p.id':id})
+}
